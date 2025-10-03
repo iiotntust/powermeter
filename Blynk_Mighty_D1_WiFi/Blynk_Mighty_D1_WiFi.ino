d201ff876227b55ac34f4e70960abb84b351bb6a
Max <110401608+Max-Chien@users.noreply.github.com>
2025-10-03 13:20:28 +0800

    Blynk_Mighty_D1_WiFi.ino

--- Blynk_Mighty_D1_WiFi/Blynk_Mighty_D1_WiFi.ino
+++ Blynk_Mighty_D1_WiFi/Blynk_Mighty_D1_WiFi.ino
@@ -1,113 +1,53 @@
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- * This example runs directly on ESP8266 chip.
- *
- * Note: This requires ESP8266 support package:
- *   https://github.com/esp8266/Arduino
- *
- * Please be sure to select the right ESP8266 module
- * in the Tools -> Board menu!
- *
- * NOTE: SmartConfig might not work in your environment.
- *       Please try basic ESP8266 SmartConfig examples
- *       before using this sketch!
- *
- * Change Blynk auth token to run :)
- * 
- * Plus WiFi-config function into Blynk Mighty D1
- *
- **************************************************************/
-
-#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL6gK4qIqxl"
+#define BLYNK_TEMPLATE_NAME "powermeter"
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-#include <SoftwareSerial.h> // Arduino IDE <1.6.6
-#include <PZEM004T.h>
+#include <SoftwareSerial.h>
+#include <PZEM004Tv30.h>
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "4e08a6a3122343f5beb5********"; //imtbtest01;
-//char auth[] = "f82b3aa574924044bcf365********"; //imtbtest02; 
+char auth[] = "ch6VCQ0nj_iVfs__J_K_k843aWt1s7Wn";
+char ssid[] = "Factory2_2.4G";
+char pass[] = "118factory2";
 
-//char ssid[] = "Classroom";  //at I40 Center;
-//char pass[] = "********";   //at I40 Center;
+// D5=RX(接TX), D6=TX(接RX)
+SoftwareSerial pzemSW(D5, D6);
+PZEM004Tv30 pzem(pzemSW);
 
-PZEM004T pzem(&Serial);
-IPAddress ip(192,168,1,1);
 
-void setup()
-{
+float voltage, current, power, energy;
+BlynkTimer timer;
+
+void setup() {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
-  pzem.setAddress(ip);
-  WiFi.mode(WIFI_STA);
-
-  int cnt = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    if (cnt++ >= 10) {
-      WiFi.beginSmartConfig();
-      while (1) {
-        delay(1000);
-        if (WiFi.smartConfigDone()) {
-          Serial.println();
-          Serial.println("SmartConfig: Success");
-          break;
-        }
-        Serial.print("|");
-      }
-    }
-  }
-
-  WiFi.printDiag(Serial);
-
-  Blynk.config(auth);
+  timer.setInterval(1000L, sendPowerData);
 }
 
-void loop()
-{
+void loop() {
+  voltage = pzem.voltage();
+  current = pzem.current();
+  power   = pzem.power();
+  energy  = pzem.energy();
   Blynk.run();
-//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);                       // wait for a second
-//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);                       // wait for a second  
+  timer.run();
 }
 
+void sendPowerData() {
+  if (isnan(voltage)) voltage = 0;
+  if (isnan(current)) current = 0;
+  if (isnan(power))   power = 0;
+  if (isnan(energy))  energy = 0;
 
-BLYNK_READ(V0) {
-  float v = pzem.voltage(ip);
-  if (v < 0.0) v = 0.0;
-  Blynk.virtualWrite(V0,v);
-  }
+  Serial.print("Voltage: "); Serial.print(voltage); Serial.println(" V");
+  Serial.print("Current: "); Serial.print(current); Serial.println(" A");
+  Serial.print("Power: ");   Serial.print(power);   Serial.println(" W");
+  Serial.print("Energy: ");  Serial.print(energy); Serial.println(" Wh");
+  Serial.println("-------------------------");
 
-BLYNK_READ(V1) {
-  float i = pzem.current(ip);
-  if (i < 0.0) i = 0.0;
-  Blynk.virtualWrite(V1,i);
-  }
+  Blynk.virtualWrite(V5, voltage);
+  Blynk.virtualWrite(V6, current);
+  Blynk.virtualWrite(V7, power);
+  Blynk.virtualWrite(V8, energy);
+}
 
-BLYNK_READ(V2) {
-  float p = pzem.power(ip);
-  if (p < 0.0) p = 0.0;
-  Blynk.virtualWrite(V2,p);
-  }
-
-BLYNK_READ(V3) {
-  float e = pzem.energy(ip);
-  if (e < 0.0) e = 0.0;
-  Blynk.virtualWrite(V3,e/1000);
-  }
